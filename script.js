ff44315ecb70052f28ae43648fbf229f364b34e4
Bash-Abdul <bashirtaiwo59@gmail.com>
2024-05-30 14:41:43 +0100

    first project commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var count = new Date("Aug 13, 2024 00:00:00");
+var x = setInterval(()=>{
+    var now = new Date().getTime();
+    var distance = count - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 *24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+    }
+
+},1000);
